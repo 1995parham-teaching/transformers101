d71ef48528594ae58686ef8d4f8c0d21bd581485
Parham Alvani <parham.alvani@gmail.com>
2024-12-06 21:09:08 +0000

    feat: add required packages

---
+++ main.py
@@ -0,0 +1,9 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model = AutoModelForCausalLM.from_pretrained(
+    "microsoft/Phi-3-mini-4k-instruct",
+    device_map="cude",
+    torch_dtype="auto",
+    trust_remote_code=True,
+)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
