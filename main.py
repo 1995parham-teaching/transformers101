8949ce4b98711bf673cf8f1162876852d4f6801f
Parham Alvani <parham.alvani@gmail.com>
2024-12-06 22:09:26 +0000

    chore: use auto as device map

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model = AutoModelForCausalLM.from_pretrained(
     "microsoft/Phi-3-mini-4k-instruct",
-    device_map="cude",
+    device_map="auto",
     torch_dtype="auto",
     trust_remote_code=True,
 )
