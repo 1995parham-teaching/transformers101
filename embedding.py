1015cb28fdf72dd6979ddfdd5793207d790217d3
Parham Alvani <parham.alvani@gmail.com>
2024-12-07 19:30:19 +0000

    feat: add new library and example of using embedding

---
+++ embedding.py
@@ -0,0 +1,10 @@
+from transformers import AutoModel, AutoTokenizer
+
+if __name__ == "__main__":
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v3-base")
+
+    model = AutoModel.from_pretrained("microsoft/deberta-v3-base")
+
+    tokens = tokenizer("Hello World", return_tensors="pt")
+
+    print(tokens)
