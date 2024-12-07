40a7fed4cbc4fea52dac19e96414efb682d6e111
Parham Alvani <parham.alvani@gmail.com>
2024-12-07 05:28:47 +0000

    feat: use prompt to generate

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ model = AutoModelForCausalLM.from_pretrained(
     trust_remote_code=True,
 )
 tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
+
+prompt = "Write an email apologizing to Sarah for the tagic gadering mishap. Eplain how it happened.<|assistant|>"
+
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+
+generation_output = model.generate(
+    input_ids=input_ids,
+    max_new_tokens=20,
+)
+
+print()
+print(tokenizer.decode(generation_output[0]))
