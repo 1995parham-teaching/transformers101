e53b76efacd2fc78382c5348702e50cebf04170b
Parham Alvani <parham.alvani@gmail.com>
2024-12-08 14:14:46 +0000

    feat: use book compatible example

--- embedding.py
+++ embedding.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     model = AutoModel.from_pretrained("microsoft/deberta-v3-base")
 
-    tokens = tokenizer("Hello World")
+    tokens = tokenizer("Hello World", return_tensors="pt")
 
-    for t in tokens.input_ids:
+    for t in tokens.input_ids[0]:
         print(tokenizer.decode(t))
