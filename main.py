f6655eb973b373c2e3be37154faea2971276c33e
Parham Alvani <parham.alvani@gmail.com>
2024-12-09 06:43:19 +0000

    feat: add pipeline for generating text

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-model = AutoModelForCausalLM.from_pretrained(
-    "microsoft/Phi-3-mini-4k-instruct",
-    device_map="auto",
-    torch_dtype="auto",
-    trust_remote_code=True,
-)
-tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
+if __name__ == "__main__":
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
+    model = AutoModelForCausalLM.from_pretrained(
+        "microsoft/Phi-3-mini-4k-instruct",
+        device_map="auto",
+        torch_dtype="auto",
+        trust_remote_code=True,
+    )
 
-prompt = "Write an email apologizing to Sarah for the tagic gadering mishap. Eplain how it happened.<|assistant|>"
+    generator = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        return_full_text=False,
+        max_new_tokens=50,
+        do_sample=False,
+    )
 
-input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+    prompt = "Write an email apologizing to Sarah for the tagic gadering mishap. Eplain how it happened.<|assistant|>"
 
-generation_output = model.generate(
-    input_ids=input_ids,
-    max_new_tokens=20,
-)
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
 
-print()
-print(tokenizer.decode(generation_output[0]))
+    generation_output = model.generate(
+        input_ids=input_ids,
+        max_new_tokens=20,
+    )
+
+    print()
+    print(tokenizer.decode(generation_output[0]))
+
+    output = generator(prompt)
+
+    print(output[0]["generated_text"])
